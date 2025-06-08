c89509661dda88bf6e9c1359378f0e1704ebb444
tildev <tildevelove@gmail.com>
2025-06-08 20:26:04 +0900

    feat: Add challenges - The brithday bar

---
+++ src/challenges/thebirthdaybar/Solution.java
@@ -0,0 +1,70 @@
+package challenges.thebirthdaybar;
+
+import java.io.*;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'birthday' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY s
+     *  2. INTEGER d
+     *  3. INTEGER m
+     */
+
+    public static int birthday(List<Integer> s, int d, int m) {
+        // Write your code here
+        // 6
+        // 1 1 1 1 1 1
+        // 3 2
+
+        int sum = 0, count = 0;
+        for (int i = 0; i < m; i++) {
+            sum += s.get(i);
+        }
+        if (sum == d) {
+            count++;
+        }
+        System.out.println(sum);
+        for (int i = m; i < s.size(); i++) {
+            sum = sum - s.get(i - m) + s.get(i);
+            if (sum == d) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> s = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int d = Integer.parseInt(firstMultipleInput[0]);
+
+        int m = Integer.parseInt(firstMultipleInput[1]);
+
+        int result = Result.birthday(s, d, m);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
