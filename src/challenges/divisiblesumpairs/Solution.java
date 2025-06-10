01e881e1accf4d43548837eacda4f04ae749af9f
tildev <tildevelove@gmail.com>
2025-06-10 14:30:16 +0900

    feat: Add challenges - Divisible sum pairs

---
+++ src/challenges/divisiblesumpairs/Solution.java
@@ -0,0 +1,67 @@
+package challenges.divisiblesumpairs;
+
+import java.io.*;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'divisibleSumPairs' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. INTEGER k
+     *  3. INTEGER_ARRAY ar
+     */
+
+    public static int divisibleSumPairs(int n, int k, List<Integer> ar) {
+        int count = 0, mod = 0, complement = 0;
+        int[] modCount = new int[k];
+
+        for (int i = 0; i < n; i++) {
+            mod = ar.get(i)%k;
+            complement = (k - mod) % k;
+            count += modCount[complement];
+            modCount[mod]++;
+        }
+
+        /*for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if ((ar.get(i) + ar.get(j)) % k == 0) {
+                    count++;
+                }
+            }
+        }*/
+        return count;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(firstMultipleInput[0]);
+
+        int k = Integer.parseInt(firstMultipleInput[1]);
+
+        List<Integer> ar = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        int result = Result.divisibleSumPairs(n, k, ar);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
